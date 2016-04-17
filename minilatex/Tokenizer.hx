4698eafdb03e0da8d073b8ce57a2b5d3de101943
ARATA Mizuki <minorinoki@gmail.com>
2016-04-17 15:03:13 +0900

    Record line and column in the Token object

--- minilatex/Tokenizer.hx
+++ minilatex/Tokenizer.hx
@@ -11,6 +11,9 @@ class Tokenizer
 {
     var input: String;
     var position: Int;
+    var filename: String;
+    var currentLine: Int;
+    var currentColumn: Int;
     var state: State;
     var rxSpaces: EReg;
     var rxToken_atletter: EReg;
@@ -39,14 +42,21 @@ class Tokenizer
             this.position += matchedPos.pos + matchedPos.len;
         #end
     }
-    public function new(input: String)
+    public function new(input: String, filename: String = "<input>")
     {
         this.input = input;
         this.position = 0;
+        this.filename = filename;
+        this.currentLine = 1;
+        this.currentColumn = 0;
         this.state = State.NewLine;
         this.rxSpaces = makeAnchoredRx("[ \t]+");
         this.rxToken_atother = makeAnchoredRx("(%.*\n?)|\\\\(?:([a-zA-Z]+)|(.|\n))|([ \t])|(\n)|([^%\\\\])");
         this.rxToken_atletter = makeAnchoredRx("(%.*\n?)|\\\\(?:([a-zA-Z@]+)|(.|\n))|([ \t])|(\n)|([^%\\\\])");
+    }
+    function getCurrentLocation(): TokenLocation
+    {
+        return {filename: this.filename, line: this.currentLine, column: this.currentColumn};
     }
     public function readToken(scope: Scope): Null<Token>
     {
@@ -54,21 +64,28 @@ class Tokenizer
         while (this.position < this.input.length) {
             if (this.state == State.NewLine || this.state == State.SkipSpaces) {
                 if (matchAnchoredRx(this.rxSpaces, this.input, this.position)) {
-                    updatePosition(this.rxSpaces.matchedPos());
+                    var matchedPos = this.rxSpaces.matchedPos();
+                    updatePosition(matchedPos);
+                    this.currentColumn += matchedPos.len; // rxSpaces does not match EOL
                     continue;
                 }
             }
             if (matchAnchoredRx(rxToken, this.input, this.position)) {
-                updatePosition(rxToken.matchedPos());
+                var matchedPos = rxToken.matchedPos();
+                var currentLocation = this.getCurrentLocation();
+                updatePosition(matchedPos);
+                this.currentColumn += matchedPos.len;
 
                 if (rxToken.matched(1) != null) { /* comment */
                     this.state = State.NewLine;
+                    ++this.currentLine;
+                    this.currentColumn = 0;
                     continue;
 
                 } else if (rxToken.matched(2) != null) { /* control word */
                     var word = rxToken.matched(2);
                     this.state = State.SkipSpaces;
-                    return new Token(ControlSequence(word), null);
+                    return new Token(ControlSequence(word), currentLocation);
 
                 } else if (rxToken.matched(3) != null) { /* control symbol */
                     var c = rxToken.matched(3);
@@ -78,31 +95,36 @@ class Tokenizer
                     } else if (c == '\n') {
                         c = ' ';
                         this.state = State.NewLine;
+                        ++this.currentLine;
+                        this.currentColumn = 0;
                     } else {
                         this.state = State.MiddleOfLine;
                     }
-                    return new Token(ControlSequence(c), null);
+                    return new Token(ControlSequence(c), currentLocation);
 
                 } else if (rxToken.matched(4) != null) { /* space */
                     this.state = State.SkipSpaces;
-                    return new Token(ControlSequence(' '), null);
+                    return new Token(ControlSequence(' '), currentLocation);
 
                 } else if (rxToken.matched(5) != null) { /* newline */
+                    var currentLocation = this.getCurrentLocation();
+                    ++this.currentLine;
+                    this.currentColumn = 0;
                     switch (this.state) {
                     case State.NewLine:
-                        return new Token(ControlSequence("par"), null);
+                        return new Token(ControlSequence("par"), currentLocation);
                     case State.SkipSpaces:
                         this.state = State.NewLine;
                         continue;
                     default:
                         this.state = State.NewLine;
-                        return new Token(Character(' '), null);
+                        return new Token(Character(' '), currentLocation);
                     }
 
                 } else if (rxToken.matched(6) != null) { /* other */
                     var c = rxToken.matched(6);
                     this.state = State.MiddleOfLine;
-                    return new Token(Character(c), null);
+                    return new Token(Character(c), currentLocation);
 
                 } else {
                     throw new LaTeXError("regexp error");
