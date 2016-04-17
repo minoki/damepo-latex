93e9b13828d1232cb0fefe3f8887d52217997f5d
ARATA Mizuki <minorinoki@gmail.com>
2016-04-17 18:31:19 +0900

    Add TokenValueExtender

--- minilatex/Token.hx
+++ minilatex/Token.hx
@@ -4,31 +4,20 @@ enum TokenValue
     Character(c: String);
     ControlSequence(name: String);
 }
-typedef TokenLocation = {
-    var filename: String;
-    var line: Int;
-    var column: Int;
-}
-class Token
+class TokenValueExtender
 {
-    public var value: TokenValue;
-    public var location: TokenLocation;
-    public function new(value: TokenValue, location: TokenLocation)
-    {
-        this.value = value;
-        this.location = location;
-    }
-    public static function tokenValueToString(value: TokenValue): String
+    public static function toString(value: TokenValue): String
     {
         return switch (value) {
         case Character(c): c;
         case ControlSequence(name): "\\" + name;
         };
     }
-    public function toString(): String
-    {
-        return tokenValueToString(this.value);
-    }
+}
+typedef TokenLocation = {
+    var filename: String;
+    var line: Int;
+    var column: Int;
 }
 class TokenLocationExtender
 {
@@ -39,5 +28,19 @@ class TokenLocationExtender
         } else {
             return "file " + location.filename + ", line " + location.line + ", column " + location.column;
         }
+    }
+}
+class Token
+{
+    public var value: TokenValue;
+    public var location: Null<TokenLocation>;
+    public function new(value: TokenValue, location: Null<TokenLocation>)
+    {
+        this.value = value;
+        this.location = location;
+    }
+    public function toString(): String
+    {
+        return TokenValueExtender.toString(this.value);
     }
 }
