1bc0e3f42976ec53e0f2e8660cb67427cf9d768e
ARATA Mizuki <minorinoki@gmail.com>
2016-04-23 00:44:36 +0900

    Split ExecutionProcessor into BasicExecutionProcessor<> and ExecutionProcessor

--- minilatex/ExecutionProcessor.hx
+++ minilatex/ExecutionProcessor.hx
@@ -30,14 +30,54 @@ interface IVerbTextExecutionProcessor extends IExecutionProcessor
 {
     function verbCommand(text: String, star: Bool): Void;
 }
-class ExecutionProcessor implements IExecutionProcessor
+class BasicExecutionProcessor<E> implements IExecutionProcessor
 {
-    public var expansionProcessor: ExpansionProcessor<ExecutionProcessor>;
+    public var expansionProcessor: ExpansionProcessor<E>;
     var environmentStack: Array<String>;
-    public function new(expansionProcessor: ExpansionProcessor<ExecutionProcessor>)
+    public function new(expansionProcessor: ExpansionProcessor<E>)
     {
         this.expansionProcessor = expansionProcessor;
         this.environmentStack = [];
+    }
+    public function getTokenizer(): Tokenizer
+    {
+        return this.expansionProcessor.tokenizer;
+    }
+    public function setAtLetter(value: Bool)
+    {
+        this.expansionProcessor.currentScope.setAtLetter(value);
+    }
+    public function getExpansionProcessor()
+    {
+        return this.expansionProcessor;
+    }
+    public function beginEnvironment(name: String)
+    {
+        this.environmentStack.push(name);
+    }
+    public function endEnvironment(name: String)
+    {
+        var lastEnv = this.environmentStack.pop();
+        if (lastEnv == null) {
+            throw new LaTeXError("No matching \\begin{" + name + "} for \\end{" + name + "}");
+        } else if (lastEnv != name) {
+            throw new LaTeXError("\\begin{" + lastEnv + "} ended by \\end{" + name + "}");
+        }
+    }
+    public function checkEnvironment()
+    {
+        if (this.environmentStack.length > 0) {
+            var name = this.environmentStack[this.environmentStack.length - 1];
+            throw new LaTeXError("\\begin{" + name + "} is not ended");
+        }
+    }
+}
+class ExecutionProcessor extends BasicExecutionProcessor<ExecutionProcessor>
+    implements IExecutionProcessor
+{
+    public function new(expansionProcessor: ExpansionProcessor<ExecutionProcessor>)
+    {
+        super(expansionProcessor);
     }
     public function processAll(): Array<ExecutionResult>
     {
@@ -82,38 +122,6 @@ class ExecutionProcessor implements IExecutionProcessor
             case ExecutableCommand(name, command):
                 command.execute(this);
             }
-        }
-    }
-    public function getTokenizer(): Tokenizer
-    {
-        return this.expansionProcessor.tokenizer;
-    }
-    public function setAtLetter(value: Bool)
-    {
-        this.expansionProcessor.currentScope.setAtLetter(value);
-    }
-    public function getExpansionProcessor()
-    {
-        return this.expansionProcessor;
-    }
-    public function beginEnvironment(name: String)
-    {
-        this.environmentStack.push(name);
-    }
-    public function endEnvironment(name: String)
-    {
-        var lastEnv = this.environmentStack.pop();
-        if (lastEnv == null) {
-            throw new LaTeXError("No matching \\begin{" + name + "} for \\end{" + name + "}");
-        } else if (lastEnv != name) {
-            throw new LaTeXError("\\begin{" + lastEnv + "} ended by \\end{" + name + "}");
-        }
-    }
-    public function checkEnvironment()
-    {
-        if (this.environmentStack.length > 0) {
-            var name = this.environmentStack[this.environmentStack.length - 1];
-            throw new LaTeXError("\\begin{" + name + "} is not ended");
         }
     }
 }
