482e74308ae16f8cc18ee701a0b8507138403950
ARATA Mizuki <minorinoki@gmail.com>
2016-04-24 13:03:38 +0900

    Simplify implementation of CodePointIterator

--- minilatex/util/UnicodeUtil.hx
+++ minilatex/util/UnicodeUtil.hx
@@ -8,75 +8,29 @@ private class CodePointIterator
         this.s = s;
         this.index = index;
     }
-    public function hasNext()
+    public inline function hasNext()
     {
         return this.index < this.s.length;
     }
     public function next()
     {
-        var x = this.s.charCodeAt(index++);
+        var x = UnicodeUtil.codePointAt(this.s, this.index++);
         #if (js || java)
-            /* decode UTF-16 sequence */
-            if (0xD800 <= x && x <= 0xDFFF) {
-                if (0xDC00 <= x) {
-                    throw "codePointAt: invalid tail surrogate";
-                }
-                var y = this.s.charCodeAt(index++);
-                if (0xDC00 <= y && y <= 0xDFFF) {
-                    return (((x & 0x3FF) << 10) + 0x10000) | (y & 0x3FF);
-                } else {
-                    throw "codePointAt: invalid tail surrogate";
-                }
-            } else {
-                return x;
+            /* UTF-16 */
+            if (x >= 0x10000) {
+                this.index++;
             }
         #elseif (cpp || neko || php)
-            /* decode UTF-8 sequence */
-            if (x < 0x80) {
-                return x;
-            } else {
-                if (x < 0xC0) {
-                    throw "codePointAt: invalid UTF-8 sequence";
-                } else if (x < 0xE0) {
-                    var x2 = this.s.charCodeAt(index++);
-                    var a0 = x & 0x1F;
-                    var a1 = x2 & 0x3F;
-                    var v = (a0 << 6) | a1;
-                    if (v < 0x80 || (x2 & 0xC0) != 0x80) {
-                        throw "codePointAt: invalid UTF-8 sequence";
-                    }
-                    return v;
-                } else if (x < 0xF0) {
-                    var x2 = this.s.charCodeAt(index++);
-                    var x3 = this.s.charCodeAt(index++);
-                    var a0 = x & 0x0F;
-                    var a1 = x2 & 0x3F;
-                    var a2 = x3 & 0x3F;
-                    var v = (a0 << 12) | (a1 << 6) | a2;
-                    if (v < 0x800 || (x2 & 0xC0) != 0x80 || (x3 & 0xC0) != 0x80) {
-                        throw "codePointAt: invalid UTF-8 sequence";
-                    }
-                    return v;
-                } else if (x < 0xF8) {
-                    var x2 = this.s.charCodeAt(index++);
-                    var x3 = this.s.charCodeAt(index++);
-                    var x4 = this.s.charCodeAt(index++);
-                    var a0 = x & 0x07;
-                    var a1 = x2 & 0x3F;
-                    var a2 = x3 & 0x3F;
-                    var a3 = x4 & 0x3F;
-                    var v = (a0 << 18) | (a1 << 12) | (a2 << 6) | a3;
-                    if (v < 0x10000 || (x2 & 0xC0) != 0x80 || (x3 & 0xC0) != 0x80 || (x4 & 0xC0) != 0x80) {
-                        throw "codePointAt: invalid UTF-8 sequence";
-                    }
-                    return v;
-                } else {
-                    throw "codePointAt: invalid UTF-8 sequence";
-                }
+            /* UTF-8 */
+            if (x >= 0x10000) {
+                this.index += 3;
+            } else if (x >= 0x800) {
+                this.index += 2;
+            } else if (x >= 0x80) {
+                this.index += 1;
             }
-        #else
-            return this.s.charCodeAt(this.index++);
         #end
+        return x;
     }
 }
 class UnicodeUtil
@@ -86,7 +40,7 @@ class UnicodeUtil
     #else // if (neko || cpp || php || flash || java)
         public static var rxSingleCodepoint = ~/^.$/us; /* s: PCRE_DOTALL */
     #end
-    public static function codePointIterator(s: String): Iterator<Int>
+    public static inline function codePointIterator(s: String): Iterator<Int>
     {
         return new CodePointIterator(s);
     }
